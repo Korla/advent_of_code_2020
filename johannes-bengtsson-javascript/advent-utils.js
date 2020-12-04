6fc9eea88c111fed5b2410abd2003f1ae3740ab6
korla <bengtsson.johannes@gmail.com>
2020-12-04 20:40:13 +0100

    Init dependencies

---
+++ johannes-bengtsson-javascript/advent-utils.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const getData = (...args) => getDataWithEmpty(...args).filter(a => a);
+exports.getData = getData;
+
+const getDataWithEmpty = (day, data) => (data || fs.readFileSync(`${day}.txt`, 'utf8')).split('\n');
+exports.getDataWithEmpty = getDataWithEmpty;
