bbf4f2feed046e9db09e5494b25e2783d2b5247c
korla <bengtsson.johannes@gmail.com>
2020-12-07 20:10:20 +0100

    Day 7 part 2

--- johannes-bengtsson-javascript/7/7.js
+++ johannes-bengtsson-javascript/7/7.js
@@ -7,24 +7,33 @@ exports.createBagSpecs = lazy.chain(
         b
             .replace(/s\.|\./, '')
             .split(',')
-            .map((a) => a.match(/([0-9]) (.*) bag/))
-            .filter((a) => !!a)
-            .map(([_, b, c]) => [Number(b), c]),
-    ])
+            .map((a) => a.match(/([0-9]) (.*) bag/)),
+    ]),
+    lazy.runGenerator(function* (seq) {
+        for (const [color, bagSpecs] of seq) {
+            for (const match of bagSpecs) {
+                if (!match) {
+                    yield [color];
+                } else {
+                    for (let i = 0; i < Number(match[1]); i++) {
+                        yield [color, match[2]];
+                    }
+                }
+            }
+        }
+    })
 );
 
 exports.getShinyContainers = lazy.chain(
     lazy.runGenerator(function* (seq) {
-        yield Array.from(exports.createBagSpecs((seq)));
+        yield Array.from(exports.createBagSpecs(seq));
     }),
     lazy.loop(),
     lazy.reduce(
         ({ containers, added }, bagSpecs) => {
             const newAdded = bagSpecs
-                .filter(([color, containedBags]) =>
-                    added.find((addedColor) =>
-                        containedBags.find(([_, color]) => addedColor === color)
-                    )
+                .filter(([color, childColor]) =>
+                    added.find((addedColor) => addedColor === childColor)
                 )
                 .map(([color]) => color);
             return {
@@ -40,4 +49,34 @@ exports.getShinyContainers = lazy.chain(
     lazy.doTakeWhile(({ added }) => added.length > 0),
     lazy.takeLast(),
     lazy.map(({ containers }) => new Set(containers).size - 1)
+);
+
+exports.getNumberOfBagsInShiny = lazy.chain(
+    lazy.runGenerator(function* (seq) {
+        yield Array.from(exports.createBagSpecs(seq)).filter(
+            ([color, childColor]) => childColor !== undefined
+        );
+    }),
+    lazy.loop(),
+    lazy.reduce(
+        ({ count, added }, bagSpecs) => {
+            const newAdded = added.reduce((newAdded, addedColor) => [
+                ...newAdded,
+                ...bagSpecs.filter(
+                    ([color, childColor]) => color === addedColor
+                ).map(([color, childColor]) => childColor),
+            ], []);
+            return {
+                count: count + newAdded.length,
+                added: newAdded,
+            };
+        },
+        {
+            count: 0,
+            added: ['shiny gold'],
+        }
+    ),
+    lazy.doTakeWhile(({ added }) => added.length > 0),
+    lazy.takeLast(),
+    lazy.map(({ count }) => count)
 );
--- johannes-bengtsson-javascript/7/7.spec.js
+++ johannes-bengtsson-javascript/7/7.spec.js
@@ -1,6 +1,10 @@
 const { getDataWithEmpty } = require('../utils/get-data');
 const { deepEqual } = require('assert');
-const { createBagSpecs, getShinyContainers } = require('./7');
+const {
+    createBagSpecs,
+    getShinyContainers,
+    getNumberOfBagsInShiny,
+} = require('./7');
 
 const testData = `light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
@@ -12,52 +16,60 @@ vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.`;
 
+const testData2 = `shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`;
+
 const bagSpecs = [
-    [
-        'light red',
-        [
-            [1, 'bright white'],
-            [2, 'muted yellow'],
-        ],
-    ],
-    [
-        'dark orange',
-        [
-            [3, 'bright white'],
-            [4, 'muted yellow'],
-        ],
-    ],
-    ['bright white', [[1, 'shiny gold']]],
-    [
-        'muted yellow',
-        [
-            [2, 'shiny gold'],
-            [9, 'faded blue'],
-        ],
-    ],
-    [
-        'shiny gold',
-        [
-            [1, 'dark olive'],
-            [2, 'vibrant plum'],
-        ],
-    ],
-    [
-        'dark olive',
-        [
-            [3, 'faded blue'],
-            [4, 'dotted black'],
-        ],
-    ],
-    [
-        'vibrant plum',
-        [
-            [5, 'faded blue'],
-            [6, 'dotted black'],
-        ],
-    ],
-    ['faded blue', []],
-    ['dotted black', []],
+    ['light red', 'bright white'],
+    ['light red', 'muted yellow'],
+    ['light red', 'muted yellow'],
+    ['dark orange', 'bright white'],
+    ['dark orange', 'bright white'],
+    ['dark orange', 'bright white'],
+    ['dark orange', 'muted yellow'],
+    ['dark orange', 'muted yellow'],
+    ['dark orange', 'muted yellow'],
+    ['dark orange', 'muted yellow'],
+    ['bright white', 'shiny gold'],
+    ['muted yellow', 'shiny gold'],
+    ['muted yellow', 'shiny gold'],
+    ['muted yellow', 'faded blue'],
+    ['muted yellow', 'faded blue'],
+    ['muted yellow', 'faded blue'],
+    ['muted yellow', 'faded blue'],
+    ['muted yellow', 'faded blue'],
+    ['muted yellow', 'faded blue'],
+    ['muted yellow', 'faded blue'],
+    ['muted yellow', 'faded blue'],
+    ['muted yellow', 'faded blue'],
+    ['shiny gold', 'dark olive'],
+    ['shiny gold', 'vibrant plum'],
+    ['shiny gold', 'vibrant plum'],
+    ['dark olive', 'faded blue'],
+    ['dark olive', 'faded blue'],
+    ['dark olive', 'faded blue'],
+    ['dark olive', 'dotted black'],
+    ['dark olive', 'dotted black'],
+    ['dark olive', 'dotted black'],
+    ['dark olive', 'dotted black'],
+    ['vibrant plum', 'faded blue'],
+    ['vibrant plum', 'faded blue'],
+    ['vibrant plum', 'faded blue'],
+    ['vibrant plum', 'faded blue'],
+    ['vibrant plum', 'faded blue'],
+    ['vibrant plum', 'dotted black'],
+    ['vibrant plum', 'dotted black'],
+    ['vibrant plum', 'dotted black'],
+    ['vibrant plum', 'dotted black'],
+    ['vibrant plum', 'dotted black'],
+    ['vibrant plum', 'dotted black'],
+    ['faded blue'],
+    ['dotted black'],
 ];
 
 describe('Day 7 tests', () => {
@@ -77,5 +89,17 @@ describe('Day 7 tests', () => {
         const data = getDataWithEmpty(7);
         const expected = [205];
         deepEqual(Array.from(getShinyContainers(data)), expected);
+    });
+
+    it('getNumberOfBagsInShiny example', () => {
+        const data = getDataWithEmpty(7, testData2);
+        const expected = [126];
+        deepEqual(Array.from(getNumberOfBagsInShiny(data)), expected);
+    });
+
+    it.only('getNumberOfBagsInShiny', () => {
+        const data = getDataWithEmpty(7);
+        const expected = [12];
+        deepEqual(Array.from(getNumberOfBagsInShiny(data)), expected);
     });
 });
