4179b020f26434796fffa2c8e053758461cc4b32
korla <bengtsson.johannes@gmail.com>
2020-12-12 14:15:47 +0100

    12 part 2

--- johannes-bengtsson-javascript/12/12.js
+++ johannes-bengtsson-javascript/12/12.js
@@ -6,15 +6,17 @@ const moves = [
     ([l, n, d]) => [n, 0, 0],
     ([l, n, d]) => [0, -n, 0],
     ([l, n, d]) => [-n, 0, 0],
-    ([l, n, d]) => [0, 0, -n/90],
-    ([l, n, d]) => [0, 0, n/90],
+    ([l, n, d]) => [0, 0, -n / 90],
+    ([l, n, d]) => [0, 0, n / 90],
     ([l, n, d]) => moves[d]([l, n]),
 ];
 
-exports.runShip = lazy.chain(
+const parseData = lazy.chain(
     lazy.map((a) => a.match(/([A-Z])(.*)/)),
-    lazy.map(([_, l, n]) => [l, Number(n)]),
-    lazy.log(0),
+    lazy.map(([_, l, n]) => [l, Number(n)])
+);
+exports.runShip = lazy.chain(
+    parseData,
     lazy.reduce(
         (prev, [l, n]) =>
             moves[moveIndexes.indexOf(l)]([l, n, prev[2]]).map((delta, i) =>
@@ -22,7 +24,29 @@ exports.runShip = lazy.chain(
             ),
         [0, 0, 1]
     ),
-    lazy.log(1),
+    lazy.takeLast(),
+    lazy.map(([x, y]) => Math.abs(x) + Math.abs(y))
+);
+
+const rotate = (x, y, times) =>
+    times > 1 ? rotate(y, -x, times - 1) : [y, -x];
+const move2 = [
+    ([l, n, x, y, dx, dy]) => [x, y, dx + n, dy],
+    ([l, n, x, y, dx, dy]) => [x, y, dx, dy + n],
+    ([l, n, x, y, dx, dy]) => [x, y, dx - n, dy],
+    ([l, n, x, y, dx, dy]) => [x, y, dx, dy - n],
+    ([l, n, x, y, dx, dy]) => [x, y, ...rotate(dx, dy, n / 90)],
+    ([l, n, x, y, dx, dy]) => [x, y, ...rotate(dx, dy, 360 - n / 90)],
+    ([l, n, x, y, dx, dy]) => [x + n * dx, y + n * dy, dx, dy],
+];
+
+exports.runShipTowards = lazy.chain(
+    parseData,
+    lazy.reduce(
+        (prev, [l, n]) =>
+            move2[moveIndexes.indexOf(l)]([l, n, ...prev, prev[3]]),
+        [0, 0, 1, 10]
+    ),
     lazy.takeLast(),
     lazy.map(([x, y]) => Math.abs(x) + Math.abs(y))
 );
--- johannes-bengtsson-javascript/12/12.spec.js
+++ johannes-bengtsson-javascript/12/12.spec.js
@@ -1,6 +1,6 @@
 const { getDataWithEmpty } = require('../utils/get-data');
 const { deepEqual } = require('assert');
-const { runShip } = require('./12');
+const { runShip, runShipTowards } = require('./12');
 
 describe('Day 12 tests', () => {
     it('runShip example', () => {
@@ -20,5 +20,24 @@ F11`
         const data = getDataWithEmpty(12);
         const expected = [1319];
         deepEqual(Array.from(runShip(data)), expected);
+    });
+
+    it('runShipTowards example', () => {
+        const data = getDataWithEmpty(
+            11,
+            `F10
+N3
+F7
+R90
+F11`
+        );
+        const expected = [286];
+        deepEqual(Array.from(runShipTowards(data)), expected);
+    });
+
+    it('runShipTowards', () => {
+        const data = getDataWithEmpty(12);
+        const expected = [62434];
+        deepEqual(Array.from(runShipTowards(data)), expected);
     });
 });
