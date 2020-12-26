22d7b7d38ca16b13595333dc4603d167e75d3c93
korla <bengtsson.johannes@gmail.com>
2020-12-26 16:14:44 +0100

    14 part 2

--- johannes-bengtsson-javascript/14/14.js
+++ johannes-bengtsson-javascript/14/14.js
@@ -19,10 +19,10 @@ exports.applyMask = (v, mask) => {
 exports.runProgram = lazy.chain(
     lazy.map((s) => s.match(/mask = (.*)/) || s.match(/mem\[(.*)\] = (.*)/)),
     lazy.reduce(
-        ({ currentMask, accumulated }, [_, first, second]) => {
-            if (!second) {
+        ({ currentMask, accumulated }, [_, targetOrMask, value]) => {
+            if (!value) {
                 return {
-                    currentMask: [...first],
+                    currentMask: [...targetOrMask],
                     accumulated,
                 };
             } else {
@@ -30,7 +30,7 @@ exports.runProgram = lazy.chain(
                     currentMask,
                     accumulated: {
                         ...accumulated,
-                        [first]: exports.applyMask(second, currentMask),
+                        [targetOrMask]: exports.applyMask(value, currentMask),
                     },
                 };
             }
@@ -41,3 +41,59 @@ exports.runProgram = lazy.chain(
     lazy.takeLast(),
     lazy.map((a) => Object.values(a).reduce((a, b) => a + b))
 );
+
+exports.runProgram2 = lazy.chain(
+    lazy.map((s) => s.match(/mask = (.*)/) || s.match(/mem\[(.*)\] = (.*)/)),
+    lazy.reduce(
+        ({ currentMask, accumulated }, [_, targetOrMask, value]) => {
+            if (!value) {
+                return {
+                    currentMask: [...targetOrMask],
+                    accumulated,
+                };
+            } else {
+                const maskedValue = exports.maskValue(
+                    targetOrMask,
+                    currentMask
+                );
+                const targets = exports.generatePermutations(maskedValue);
+                const valueToWrite = Number(value);
+                return {
+                    currentMask,
+                    accumulated: {
+                        ...accumulated,
+                        ...targets.reduce(
+                            (prev, curr) => ({
+                                ...prev,
+                                [curr]: valueToWrite,
+                            }),
+                            {}
+                        ),
+                    },
+                };
+            }
+        },
+        { currentMask: null, accumulated: {} }
+    ),
+    lazy.takeLast(),
+    lazy.map(({ accumulated }) =>
+        Object.values(accumulated).reduce((a, b) => a + b)
+    )
+);
+
+exports.generatePermutations = (mask) => {
+    const firstX = mask.indexOf('X');
+    if (firstX < 0) {
+        return [mask];
+    }
+
+    return [
+        ...exports.generatePermutations(mask.replace('X', 0)),
+        ...exports.generatePermutations(mask.replace('X', 1)),
+    ];
+};
+
+exports.maskValue = (v, mask) => {
+    const value = [...Number(v).toString(2).padStart(36, '0')];
+    return mask.map((m, i) => (m === '0' ? value[i] : m)).join('');
+};
--- johannes-bengtsson-javascript/14/14.spec.js
+++ johannes-bengtsson-javascript/14/14.spec.js
@@ -1,20 +1,73 @@
 const { getDataWithEmpty } = require('../utils/get-data');
 const { deepEqual } = require('assert');
-const { runProgram } = require('./14');
+const {
+    runProgram,
+    generatePermutations,
+    maskValue,
+    runProgram2,
+} = require('./14');
 
 const example = `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0`;
 
+const example2 = `mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1`;
+
 describe('Day 14 tests', () => {
-    it('parseData', () => {
+    it('runProgram example', () => {
         const data = getDataWithEmpty(14, example);
         deepEqual(Array.from(runProgram(data)), [165]);
     });
 
-    it('parseData', () => {
+    it('runProgram', () => {
         const data = getDataWithEmpty(14);
         deepEqual(Array.from(runProgram(data)), [13556564111697]);
+    });
+
+    it('generatePermutations', () => {
+        const data = '000000000000000000000000000000X1101X';
+        const expected = [
+            '000000000000000000000000000000011010',
+            '000000000000000000000000000000011011',
+            '000000000000000000000000000000111010',
+            '000000000000000000000000000000111011',
+        ];
+        deepEqual(generatePermutations(data), expected);
+    });
+
+    it('generatePermutations 2', () => {
+        const data = '00000000000000000000000000000001X0XX';
+        const expected = [
+            '000000000000000000000000000000010000',
+            '000000000000000000000000000000010001',
+            '000000000000000000000000000000010010',
+            '000000000000000000000000000000010011',
+            '000000000000000000000000000000011000',
+            '000000000000000000000000000000011001',
+            '000000000000000000000000000000011010',
+            '000000000000000000000000000000011011',
+        ];
+        deepEqual(generatePermutations(data), expected);
+    });
+
+    it('maskValue', () => {
+        const value = 42;
+        const mask = [...'000000000000000000000000000000X1001X'];
+        const expected = '000000000000000000000000000000X1101X';
+        deepEqual(maskValue(value, mask), expected);
+    });
+
+    it('runProgram2 example', () => {
+        const data = getDataWithEmpty(14, example2);
+        deepEqual(Array.from(runProgram2(data)), [208]);
+    });
+
+    it('runProgram2', () => {
+        const data = getDataWithEmpty(14);
+        deepEqual(Array.from(runProgram2(data)), [4173715962894]);
     });
 });
